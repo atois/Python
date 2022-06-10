8391bf607ed7d3b697af35391edcdd80f6a14e2c
atois <atois@hotmail.com>
2022-06-10 18:07:52 +0800

    0610

---
+++ app2.py
@@ -0,0 +1,4 @@
+character_name = "Jhon"
+character_age = "35"
+print("there once was a man named " + character_name + ",")
+print("age is " + character_age + ".")
---
+++ app3.py
@@ -0,0 +1,9 @@
+phrase = "Giraffe Academy"
+
+print(phrase + " is cool")
+
+print(phrase.lower())
+
+print("Giraffe\nAcademy")
+
+print("Giraffe\"Academy")
